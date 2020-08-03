efa11a1e6b9f134714f778be71c6686e39a8c3f0
Anthony Horton <anthorton247@gmail.com>
2020-08-03 01:09:37 -0400

    Inventory

---
+++ Week1/commerceStore/Models/inventory.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const inventorySchema = new Schema({
+    name: String,
+    cost: Number
+})
+
+module.exports = mongoose.model("Inventory", inventorySchema)
---
+++ Week1/commerceStore/server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+
+app.use(express.json())
+app.use(morgan("dev"))
+
+mongoose.connect('mongodb:/localhost:/27017/inventorydb',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    () => console.log("Connected to DB")
+)
+
+app.use('/inventory', require("./Routes/invRouter"))
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({errMsg: err.message})
+})
+
+app.listen(3000, () => {
+    console.log("App is listening on port 3000")
+})
